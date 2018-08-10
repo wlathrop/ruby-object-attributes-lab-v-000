8213648a84c53929c0f1d5c4899c29c73096d994
William Lathrop <wlathrop10@gmail.com>
2018-08-10 22:10:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,24 +40,4 @@ end
   
   
   
-  
-  class Person
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
- 
-  def name=(new_name)
-    @name = new_name
-  end
- 
-end
-  
-  
-  
-  
  
