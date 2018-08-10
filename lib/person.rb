04efb7c0957fcd6486ae00f5d1afecba49e19265
William Lathrop <wlathrop10@gmail.com>
2018-08-10 22:15:20 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,17 @@
 class Person
   
+   def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def name
+    @name 
+  end
+  def job=(job)
+    @bjob = job
+  end
+  
+  def job
+    @job
+  end 
+end
